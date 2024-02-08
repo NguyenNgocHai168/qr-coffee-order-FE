17020ebd46722f7f02eabc92aad21b4aa1ecd201
NguyenNgocHaik2 <haiboonguyenk2@gmail.com>
2024-02-08 12:13:08 +0700

    fixbug - layout v1

--- src/components/OrderList/index.tsx
+++ src/components/OrderList/index.tsx
@@ -42,7 +42,7 @@ const OrderList = () => {
                             ></th>
                         </tr>
                     </thead>
-                    <tbody className="align-baseline">
+                    {/* <tbody className="align-baseline">
                         {orderCoffeeList.map((orderCoffee: any, index: number) => (
                             <tr key={index} className="group cursor-pointer hover:bg-gray-100">
                                 <td
@@ -75,11 +75,11 @@ const OrderList = () => {
                                 </td>
                             </tr>
                         ))}
-                    </tbody>
+                    </tbody> */}
                 </table>
 
                 {/* ------------------------------------------- */}
-                <div className="block sm:hidden">
+                {/* <div className="block sm:hidden">
                     {orderCoffeeList.map((item: any, index: number) =>
                         <Card
                             key={index}
@@ -104,7 +104,7 @@ const OrderList = () => {
                     <div className="total-price my-4">
                         <p>Tổng Thành Tiền: <span className="text-grey-200">245.000.000 VNĐ</span></p>
                     </div>
-                </div>
+                </div> */}
 
                 <button type="button" className="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:focus:ring-yellow-900">Thanh Toán Tiền Mặt</button>
                 <button type="button" className="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:focus:ring-yellow-900">VNpay</button>
